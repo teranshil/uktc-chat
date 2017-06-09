1a6ce860fb29129ddee816a6987d208e07f8c0fc
Krasimir Alipiev <krasimir.alipiev@ecommera.com>
2017-06-09 15:26:18 +0300

    Fixed server routing.

--- Middlewares/router.js
+++ Middlewares/router.js
@@ -7,9 +7,17 @@ router.get('/', function(req, res){
 	res.send('home');
 });
 
+router.get('/register', function(req, res) {
+	res.send('register in progress!');
+});
+
+router.get('*', function(req, res) {
+	res.send('not found!');
+});
+
 // router.post('/login', accountController.doLogin);
 // router.post('/register', accountController.doRegister);
 // router.get('/register', accountController.register);
-// router.post('/anonymous', accountController.anonymous);	
+// router.post('/anonymous', accountController.anonymous);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var bodyParser = require('body-parser');
 var router = require('./Middlewares/router');	//include router
 const port = 4444;
 
-app.use('/', router); 	// use the router middleware
+app.use(router); 	// use the router middleware
 
-app.listen(port, (err) => {  
+app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err);
   }
