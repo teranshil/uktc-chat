088eed34589bc0aab86509bcfdf71e05c3da1698
dikel <dikelito@tutamail.com>
2017-06-13 15:30:10 +0300

    Update chatroomsController

--- controllers/chatroomsController.js
+++ controllers/chatroomsController.js
@@ -1,18 +1,29 @@
+var mysql = require('mysql');
+var db = require('../db');
+var bodyParser = require('body-parser');
+
 function index(){
     
 };
-function createRoom(){
 
+function createRoom(req, res) {
+	name = req.body.name;
+	db.createRoom(name);
+	console.log("Room created!!!");
 };
+
 function sendInvitation(){
 
 };
+
 function sendMessage(){
 
 };
+
 function chatRoomMessages(){
 
 };
+
 exports.index = index; 
 exports.createRoom = createRoom;
 exports.sendInvitation = sendInvitation;
